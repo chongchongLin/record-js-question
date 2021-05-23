e5abda64172d34f046f7ad30c7c704893278df24
lin98 <1726928218@qq.com>
2021-05-23 18:30:14 +0800

    docs:新增40-50

---
+++ 2021-05-22/41.js
@@ -0,0 +1,8 @@
+// 输出是什么？
+!!null
+!!''
+!!1
+
+//我的答案 false true  true 
+// false false true
+//’‘是falsy !'' = true
---
+++ 2021-05-22/50.js
@@ -0,0 +1,6 @@
+[1, 2, 3].map(num => {
+    if (typeof num === "number") return;
+    return num * 2;
+  });
+
+  //【undefined*3】
